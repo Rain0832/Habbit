6c666cf347cadcc04d6a2047f6710ab05209f011
Rain0832 <2710007824@qq.com>
2025-04-27 23:27:34 +0800

    add original files

---
+++ Habit_v3/new_Habit/Pomodoro.h
@@ -0,0 +1,16 @@
+﻿#ifndef POMODORO_H
+#define POMODORO_H
+
+#include <string>
+#include "Times.h"
+
+struct Pomodoro
+{
+    int id;
+    Time pomodoro_time;         // 番茄钟时间
+    std::string record;         // 番茄钟记录
+
+    Pomodoro(int id = 0, Time pomodoro_time = Time(), std::string record = "");
+};
+
+#endif // POMODORO_H
